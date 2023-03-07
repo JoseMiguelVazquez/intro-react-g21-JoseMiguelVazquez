ab12fb9a589fd4b0ed47fed4b35a75cc65144814
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-03-06 21:32:23 -0600

    Linter con Standard JS

--- 01-vite-vanilla/src/components/Countries.js
+++ 01-vite-vanilla/src/components/Countries.js
@@ -1,16 +1,16 @@
-import getCountryName from "../services"
+import getCountryName from '../services'
 
-const app = document.getElementById("app");
-const p = document.createElement("p");
+const app = document.getElementById('app')
+const p = document.createElement('p')
 
 const Countries = () => {
-    getCountryName().then(data => {
-        p.textContent = JSON.stringify(data[0].name.official);
-        app.appendChild(p);
-    })
-    getCountryName()
+  getCountryName().then(data => {
+    p.textContent = JSON.stringify(data[0].name.official)
+    app.appendChild(p)
+  })
+  getCountryName()
 
-    return ``
+  return ''
 }
 
 export default Countries
--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,12 +1,8 @@
 import './styles/style.css'
-import javascriptLogo from './assets/javascript.svg'
 import HolaMundo from './components/HolaMundo'
 import Countries from './components/Countries'
-
 
 document.querySelector('#app').innerHTML = `
   ${HolaMundo()}
   ${Countries()}
 `
-
-
