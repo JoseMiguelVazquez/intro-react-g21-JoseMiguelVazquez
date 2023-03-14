9dcc149575fdc780ec8e5000a7dda4a49660e50f
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-03-13 20:00:27 -0600

    basic todo list

--- 03-todo-list/src/components/ToDoList.jsx
+++ 03-todo-list/src/components/ToDoList.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ToDoList = () => {
+  const [inputValue, setinputValue] = useState('')
+  const handleAdd = () => {
+    console.log('Agregué', inputValue)
+  }
+
   return (
     <div>
-      ToDoList
+      <h1>Lista de Tareas</h1>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(event) => setinputValue(event.target.value)}
+      />
+      <button className='add-button' onClick={handleAdd}>Agregar</button>
+      <ul>
+        <li>Item 1
+          <button>Eliminar</button>
+        </li>
+        <li>Item 2
+          <button>Eliminar</button>
+        </li>
+        <li>Item 3
+          <button>Eliminar</button>
+        </li>
+      </ul>
     </div>
   )
 }
